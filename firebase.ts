47a1ebd8f464481d5587971269ecc4b142ef5ddf
Anirudh Dabral <anirudhdabral49@gmail.com>
2024-06-09 20:26:13 +0530

    firebase config

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkKeX2IZRQUwpRo6yOZGIPiYbFev4GERg",
+  authDomain: "kanban-app-anirudhdabral.firebaseapp.com",
+  projectId: "kanban-app-anirudhdabral",
+  storageBucket: "kanban-app-anirudhdabral.appspot.com",
+  messagingSenderId: "421848937026",
+  appId: "1:421848937026:web:9ef2b8a6e98d71c09af3db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
