588a68d311efe7c73572726491df172c6775dc35
Anirudh Dabral <anirudhdabral49@gmail.com>
2024-06-11 13:27:07 +0530

    updated favion, fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { RiHeartFill } from 'react-icons/ri'
-import { KanbanBoard } from './components/KanbanBoard'
+import { RiHeartFill } from "react-icons/ri";
+import { KanbanBoard } from "./components/KanbanBoard";
 
 function App() {
-
   return (
     <>
       <KanbanBoard />
-      <span className='fixed bottom-0 right-0 pr-2 mb-1 text-gray-500 flex'>made with React + TS + <RiHeartFill className='mt-1 text-red-600' /></span>
+      <span className="fixed bottom-0 right-0 pr-2 mb-1 text-gray-400 flex">
+        made with React + TS + ❤️
+      </span>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,71 +1,105 @@
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
-import { useState } from 'react';
-import { FaTrash } from 'react-icons/fa6';
-import { Id, Task } from '../types';
-
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
+import { FaTrash } from "react-icons/fa6";
+import { Id, Task } from "../types";
 
 interface Props {
   task: Task;
   deleteTask: (id: Id) => void;
-  updateTask: (id: Id, content: string) => void
+  updateTask: (id: Id, content: string) => void;
 }
 
 export const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
-  const [mouseIsOver, setMouseIsOver] = useState(false)
-  const [editMode, setEditMode] = useState(false)
+  const [mouseIsOver, setMouseIsOver] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const toggleEditMode = () => {
-    setEditMode(prev => !prev);
+    setEditMode((prev) => !prev);
     setMouseIsOver(false);
-  }
+  };
 
-  const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
     id: task.id,
     data: {
-      type: 'Task',
-      task
+      type: "Task",
+      task,
     },
-    disabled: editMode
-  })
+    disabled: editMode,
+  });
 
   const style = {
     transition,
-    transform: CSS.Transform.toString(transform)
-  }
+    transform: CSS.Transform.toString(transform),
+  };
 
   if (isDragging) {
-    return <div ref={setNodeRef}
-      style={style} className='bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500 cursor-grab relative opacity-30' />
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500 cursor-grab relative opacity-30"
+      />
+    );
   }
 
   if (editMode) {
     return (
-      <div ref={setNodeRef}
+      <div
+        ref={setNodeRef}
         style={style}
         {...attributes}
-        {...listeners} className='bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative'>
-        <textarea className='
+        {...listeners}
+        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+      >
+        <textarea
+          className="
     h-[90%]
     w-full resize-none border-none rounded bg-transparent text-white focus:outline-none
-    ' value={task.content} autoFocus placeholder='Task content here' onBlur={toggleEditMode} onKeyDown={(e) => {
-            if (e.key === 'Enter' && e.shiftKey) toggleEditMode()
+    "
+          value={task.content}
+          autoFocus
+          placeholder="Task content here"
+          onBlur={toggleEditMode}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.shiftKey) toggleEditMode();
           }}
-          onChange={e => updateTask(task.id, e.target.value)}></textarea>
-      </div>)
+          onChange={(e) => updateTask(task.id, e.target.value)}
+        ></textarea>
+      </div>
+    );
   }
 
   return (
-    <div ref={setNodeRef}
+    <div
+      ref={setNodeRef}
       style={style}
       {...attributes}
-      {...listeners} onClick={toggleEditMode} className='bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative' onMouseEnter={() => setMouseIsOver(true)} onMouseLeave={() => setMouseIsOver(false)}>
-      <p className='my-auto h-[90%] w-full overflow-x-hidden overflow-y-auto whitespace-pre-wrap'>
-
-        {task.content}
+      {...listeners}
+      onClick={toggleEditMode}
+      className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+      onMouseEnter={() => setMouseIsOver(true)}
+      onMouseLeave={() => setMouseIsOver(false)}
+    >
+      <p className="my-auto h-[90%] w-full overflow-x-hidden overflow-y-auto whitespace-pre-wrap">
+        {task.content.length == 0 ? "New Task" : task.content}
       </p>
-      {mouseIsOver && <button onClick={() => {
-        deleteTask(task.id);
-      }} className="text-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor rounded p-2 opacity-60 hover:opacity-100"><FaTrash /></button>}
+      {mouseIsOver && (
+        <button
+          onClick={() => {
+            deleteTask(task.id);
+          }}
+          className="text-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor rounded p-2 opacity-60 hover:opacity-100"
+        >
+          <FaTrash />
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
